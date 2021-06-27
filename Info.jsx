2aa0f5987124e3b8d96b4d786aec1c87fdea4c13
techie2040 <57973607+techie2040@users.noreply.github.com>
2021-06-27 16:19:56 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic javascript and reactjs Bootcamp</p>
+    </div>
+  );
+}
+
+
+export default Info;
